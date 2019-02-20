1f225fde4d9ce44ec1f46e8ee1422adba2c457cc
Simon Tucker <simon@brotherlogic@gmail>
2019-02-20 08:34:18 -0800

    Fixes dropboxsync. This closes brotherlogic/buildserver#286.

--- dropboxsyncapi.go
+++ dropboxsyncapi.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"time"
-
 	"golang.org/x/net/context"
 
 	pb "github.com/brotherlogic/dropboxsync/proto"
-	pbt "github.com/brotherlogic/tracer/proto"
 )
 
 //AddSyncConfig adds a basic sync config
 func (s *Server) AddSyncConfig(ctx context.Context, req *pb.AddSyncConfigRequest) (*pb.AddSyncConfigResponse, error) {
-	ctx = s.LogTrace(ctx, "AddSyncConfig", time.Now(), pbt.Milestone_START_FUNCTION)
 	s.config.SyncConfigs = append(s.config.SyncConfigs, req.ToAdd)
 	s.save(ctx)
 
-	s.LogTrace(ctx, "AddSyncConfig", time.Now(), pbt.Milestone_END_FUNCTION)
 	return &pb.AddSyncConfigResponse{}, nil
 }
--- dropboxsyncutils.go
+++ dropboxsyncutils.go
@@ -8,7 +8,6 @@ import (
 	"golang.org/x/net/context"
 
 	pb "github.com/brotherlogic/dropboxsync/proto"
-	pbt "github.com/brotherlogic/tracer/proto"
 )
 
 func stripFile(f string) string {
@@ -81,12 +80,8 @@ func diffFileList(master, new []string) []string {
 
 func (s *Server) runUpdate(ctx context.Context, config *pb.SyncConfig) {
 	t := time.Now()
-	s.LogTrace(ctx, "prelist-1", time.Now(), pbt.Milestone_START_EXTERNAL)
 	source, err := s.dropbox.listFiles(config.Key, config.Origin)
-	s.LogTrace(ctx, "postlist-1", time.Now(), pbt.Milestone_END_EXTERNAL)
-	s.LogTrace(ctx, "prelist-2", time.Now(), pbt.Milestone_START_EXTERNAL)
 	dest, err2 := s.dropbox.listFiles(config.Key, config.Destination)
-	s.LogTrace(ctx, "postlist-2", time.Now(), pbt.Milestone_END_EXTERNAL)
 	s.listTime = time.Now().Sub(t)
 
 	if err != nil || err2 != nil {
@@ -94,13 +89,10 @@ func (s *Server) runUpdate(ctx context.Context, config *pb.SyncConfig) {
 		return
 	}
 	diffs := diffFileListClever(dest, source)
-	s.LogTrace(ctx, fmt.Sprintf("diffed-%v", len(dest)), time.Now(), pbt.Milestone_MARKER)
 
 	for _, diff := range diffs {
 		s.Log(fmt.Sprintf("Copying %v to %v", diff, config.Destination+"/"+stripFile(diff)))
-		s.LogTrace(ctx, "precopy", time.Now(), pbt.Milestone_START_EXTERNAL)
 		err = s.dropbox.copyFile(config.Key, diff, config.Destination+"/"+stripFile(diff))
-		s.LogTrace(ctx, "postcopy", time.Now(), pbt.Milestone_END_EXTERNAL)
 		if err != nil {
 			s.Log(fmt.Sprintf("Error copying files: %v", err))
 		} else {
