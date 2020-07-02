9b12d4af012ea087939425e58843d8c3ceb16f2a
Simon Tucker <brotherlogic@gmail.com>
2020-07-02 16:36:02 -0700

    bouncer

--- dropboxsync.go
+++ dropboxsync.go
@@ -95,7 +95,9 @@ func (s *Server) Mote(ctx context.Context, master bool) error {
 
 // GetState gets the state of the server
 func (s *Server) GetState() []*pbg.State {
-	return []*pbg.State{}
+	return []*pbg.State{
+		&pbg.State{Key: "thing", Value: int64(1)},
+	}
 }
 
 func (s *Server) runAllUpdates(ctx context.Context) (time.Time, error) {
